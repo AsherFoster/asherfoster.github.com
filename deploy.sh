a22beaaedeba450cc1a7642cdb294b542b69be53
Asher Foster <me@asherfoster.com>
2017-06-17 23:14:07 +1200

    Something isn't working.

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,18 @@
 # Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 # I mean, that would have worked fine, but reasons.
 #
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
 TARGET_BRANCH="gh-pages"
 SOURCE_BRANCH="master"
 
 
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
+echo "REPO: ${REPO}"
+echo "SSH_REPO: ${SSH_REPO}"
 echo "SOURCE_BRANCH: ${SOURCE_BRANCH}"
 echo "TARGET_BRANCH: ${TARGET_BRANCH}"
 
@@ -17,10 +23,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; 
   exit 0
 fi
 
-# Save some useful information
-REPO=`git config remote.origin.url`
-SSH_REPO=REPO
-SHA=`git rev-parse --verify HEAD`
+
 
 
 git clone $REPO dist
