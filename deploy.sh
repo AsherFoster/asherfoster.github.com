2e35cb28043bc082503e2315b2329e331ec8498f
Asher Foster <me@asherfoster.com>
2017-06-17 22:37:50 +1200

    Make sure to read your code before deploying it to prod, folks

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@
 # Adapted from https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 # I mean, that would have worked fine, but reasons.
 #
+TARGET_BRANCH="master"
+
 
 echo "TRAVIS_PULL_REQUEST: ${TRAVIS_PULL_REQUEST}"
 echo "TRAVIS_BRANCH: ${TRAVIS_BRANCH}"
 echo "SOURCE_BRANCH: ${SOURCE_BRANCH}"
+echo "TARGET_BRANCH: ${TARGET_BRANCH}"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $TARGET_BRANCH ]; then
   echo "Skipping Deployment"
   exit 0
 fi
@@ -20,25 +23,35 @@ REPO=`git config remote.origin.url`
 SSH_REPO=REPO
 SHA=`git rev-parse --verify HEAD`
 
+
 git clone $REPO dist
 cd dist
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-rm -rf out/**/* || exit 0
+# Clear out the dist folder
+rm -rf dist/**/* || exit 0
 
+# Actually build the stuff
 npm run build
+
+
+echo "BUILD COMPLETED: NOW DEPLOYING"
+
 
 cd dist
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# Commit the "changes", i.e. the new version.
-# The delta will show diffs between new and old versions.
+# Commit the changes
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
+# Adding the ssh key from the decrypted file
+echo "ADDING SSH KEY"
+eval `ssh-agent -s`
 ssh-add deployment/key_rsa
 
 # Now that we're all set up, we can push.
+echo "PUSHING TO ${SSH_REPO} // ${TARGET_BRANCH}"
 git push $SSH_REPO $TARGET_BRANCH
