f1a00c5a610fe58a4092cd33becb47bef9cffb46
Asher Foster <me@asherfoster.com>
2017-06-18 15:18:06 +1200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,39 +24,17 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != $SOURCE_BRANCH ]; 
   exit 0
 fi
 
-
-
-# Initialize this dist directory
-echo "INITIALIZING DIST DIRECTORY"
-git clone $REPO dist
-cd dist
-git checkout -b $TARGET_BRANCH origin/$TARGET_BRANCH
-#git checkout $TARGET_BRANCH
-cd ..
-
 # Actually build the stuff
 echo "RUNNING BUILD"
-npm run build
+ng build --prod --base-href="https://asherfoster.com"
 
 
-echo "BUILD COMPLETED: NOW DEPLOYING"
 
-
-cd dist
-git config user.name "Travis CI"
-git config user.email "travis@asherfoster.com"
-git remote add orgin $SSH_REPO
-
-# Commit the changes
-echo "ACTUALLY COMMITTING THE CHANGES"
-git add -A .
-git commit -m "Deploy to GitHub Pages: ${SHA}"
 
 # Adding the ssh key from the decrypted file
 echo "ADDING SSH KEY"
 eval `ssh-agent -s`
 ssh-add ../deployment/key_rsa
 
-# Now that we're all set up, we can push.
-echo "PUSHING TO: ${SSH_REPO} // ${TARGET_BRANCH}"
-git push origin $TARGET_BRANCH
+echo "BUILD COMPLETED: NOW DEPLOYING"
+angular-cli-ghpages --message="Automatic Deployment via Travis CI"
