ec3ed9ddb1e13c146787bcbf657de6db2b14adec
Asher Foster <me@asherfoster.com>
2019-05-28 22:38:52 +1200

    Whoops

--- pages/things/whatthefuckshouldiwatch.tsx
+++ pages/things/whatthefuckshouldiwatch.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PageLayout from '../../components/PageLayout';
-import Wip from '../../components/Wip';
 import PageContent from '../../components/PageContent';
 
 const Contact = () => (
