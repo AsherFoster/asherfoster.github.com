56d4cd303d52bf5c0e22995ae855108fe93d3846
Asher Foster <me@asherfoster.com>
2019-11-28 14:28:49 +1300

    Display images better

--- components/PageContent.tsx
+++ components/PageContent.tsx
@@ -5,8 +5,14 @@ const PageContent = (props: React.ComponentProps<any>) => (
     maxWidth: '900px',
     padding: '15px',
     margin: 'auto'
-  }}>
+  }} className='main'>
     {props.children}
+    <style jsx>{`
+      .main > :global(img) {
+        max-width: 100%;
+        text-align: center;
+      }
+    `}</style>
   </main>
 );
 
--- pages/things/evolution.tsx
+++ pages/things/evolution.tsx
@@ -8,31 +8,22 @@ const Evolution = () => (
       <h2>I've iterated though far too many websites</h2>
       <p>Every time I want to test out a new tech, or just get sick of the last one, I update this site. They've been built with everything
         from PHP, Plain HTML/CSS, Angular, and finally React/Next. I'm sure this isn't the last version, but it's definitely the best.</p>
-      <div className='evolution-images'>
-        <img src='/static/history/mk1.jpg' alt='Mark 1' />
-        <caption>Even in 2015 I made sure it had a dark theme</caption>
-        <img src='/static/history/mk2.jpg' alt='Mark 2' />
-        <caption>2016. I went through a photography phase. I'm sorry.</caption>
-        <img src='/static/history/mk3.jpg' alt='Mark 3' />
-        <caption>Finally realised I sucked at design, and used a Material UI framework</caption>
-        <img src='/static/history/mk4.jpg' alt='Mark 4' />
-        <caption>2017. Although this looks similar to the previous one, it was completely rewriten in Angular</caption>
-        <img src='/static/history/mk5.jpg' alt='Mark 5' />
-        <caption>2019. First gen with Next.js. This one was... a little strange. Unfortunately, git never forgets.</caption>
-        <img src='/static/history/mk6.jpg' alt='Mark 6' />
-        <caption>Mk6 "Hip and cool". Serif fonts are hard to pull off</caption>
-      </div>
+      <img src='/static/history/mk1.jpg' alt='Mark 1' />
+      <p>Even in 2015 I made sure it had a dark theme</p>
+      <img src='/static/history/mk2.jpg' alt='Mark 2' />
+      <p>2016. I went through a photography phase. I'm sorry.</p>
+      <img src='/static/history/mk3.jpg' alt='Mark 3' />
+      <p>Finally realised I sucked at design, and used a Material UI framework</p>
+      <img src='/static/history/mk4.jpg' alt='Mark 4' />
+      <p>2017. Although this looks similar to the previous one, it was completely rewriten in Angular</p>
+      <img src='/static/history/mk5.jpg' alt='Mark 5' />
+      <p>2019. First gen with Next.js. This one was... a little strange. Unfortunately, git never forgets.</p>
+      <img src='/static/history/mk6.jpg' alt='Mark 6' />
+      <p>Mk6 "Hip and cool". Serif fonts are hard to pull off</p>
     </PageContent>
     <style jsx>{`
-    .evolution-images > img {
-      max-width: 100%;
-      text-align: center;
-    }
-    .evolution-images > caption {
-      text-align: center;
-      font-style: italic;
-      display: block;
-      margin-bottom: 2em;
+    img {
+      margin-top: 2em;
     }
     `}</style>
   </PageLayout>
